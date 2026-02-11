9810be8f40c06e59e6e9d85cc64a71f3e3b6ea58
ProjectXDz <bbbvx112233@gmail.com>
2026-02-12 02:38:50 +0700

    full project base

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Yippeeee! Supakorn ";
 
     catImg.src = "cat_dance.gif";
 
